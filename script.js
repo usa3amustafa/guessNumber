ab5b190cdef65ec8e1ebb35f1c64918b2a4774a1
usa3amustafa <usa3a3ustafa@outlook.com>
2021-07-26 14:06:38 +0500

    refoactored the code

--- script.js
+++ script.js
@@ -1,53 +1,102 @@
 'use strict';
 
-const checkBtn = document.querySelector('.check')
-const againBtn = document.querySelector('.again')
-const guessNumber = document.querySelector('.guess')
-const message = document.querySelector('.message')
-const score = document.querySelector('.score')
-const number = document.querySelector('.number')
-let highScore = document.querySelector('.highscore')
-let randomNumber
+const checkBtn = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+const guessNumber = document.querySelector('.guess');
+const message = document.querySelector('.message');
+const scoreT = document.querySelector('.score');
+const number = document.querySelector('.number');
+let highScore = document.querySelector('.highscore');
+let score = 20;
+let randomNumber;
 
+//random number function
 const randomNumberGen = () => {
-    randomNumber = Math.floor((Math.random() * 20 + 1)) 
-    console.log(randomNumber);
-} 
-    
+  randomNumber = Math.floor(Math.random() * 20 + 1);
+  console.log(randomNumber);
+};
 
+// reset function
 const reset = () => {
-    randomNumberGen()
-    guessNumber.value = ''
-    number.textContent = "?"
-    document.body.style.backgroundColor = "#222";
-    message.textContent = 'Start guessing...'
-    score.textContent = 20
-}
+  randomNumberGen();
+  guessNumber.value = '';
+  number.textContent = '?';
+  number.style.width = '15rem';
+  document.body.style.backgroundColor = '#222';
+  displayMessage('Start guessing...');
+  score = 20;
+  scoreT.textContent = score;
+  // display check button
+  checkBtnf('block');
+};
 
-randomNumberGen()
+// display message
+const displayMessage = messageA => {
+  message.textContent = messageA;
+};
 
+// check button
+const checkBtnf = property => {
+  checkBtn.style.display = property;
+};
 
-checkBtn.addEventListener('click' , function(){
-    if(!guessNumber.value){
-        message.textContent = '⛔ No Number'
-    }else if(randomNumber === Number(guessNumber.value)){
-        document.body.style.backgroundColor = "#60b347";
-        number.textContent = randomNumber
-        message.textContent = '🎉 Correct Number'
-        score.textContent > highScore.textContent ? highScore.textContent = score.textContent : highScore.textContent 
+// game over
+const gameOver = () => {
+  displayMessage('😟 You have lost the game');
+  scoreT.textContent = 0;
+  checkBtnf('none');
+};
+
+// decrease score
+const decreaseScore = () => {
+  score--;
+  scoreT.textContent = score;
+};
+
+randomNumberGen();
+
+// click event on check button
+checkBtn.addEventListener('click', function () {
+  // if there is no number
+  if (!guessNumber.value) {
+    displayMessage('⛔ No Number');
+    // if player wins
+  } else if (randomNumber === Number(guessNumber.value)) {
+    document.body.style.backgroundColor = '#60b347';
+    number.textContent = randomNumber;
+    number.style.width = '30rem';
+    displayMessage('🎉 Correct Number');
+    checkBtnf('none');
+    // if current score is greater then high score
+    score > highScore.textContent
+      ? (highScore.textContent = score)
+      : (highScore.textContent = highScore.textContent);
+  }
+  // if guessed number is low
+  else if (Number(guessNumber.value) < randomNumber) {
+    // if score is greater than zero
+    if (score > 1) {
+      displayMessage('👇🏻 Too Low');
+      decreaseScore();
+
+      // if score is zero
+    } else {
+      gameOver();
     }
-    else if(Number(guessNumber.value) < randomNumber ){
-        message.textContent = '👇🏻 Too Low'
-        score.textContent -=  1
+  } else if (Number(guessNumber.value) > randomNumber) {
+    // if guessed number is low
+    if (score > 1) {
+      displayMessage('☝🏻 Too HIGH');
+      decreaseScore();
 
+      // if score is zero
+    } else {
+      gameOver();
     }
-    else if(Number(guessNumber.value) > randomNumber ){
-        message.textContent = '☝🏻 Too HIGH'
-        score.textContent -=  1
+  }
+});
 
-    }
-})
-
-againBtn.addEventListener('click' , function(){
-    reset()
-})
+// again button
+againBtn.addEventListener('click', function () {
+  reset();
+});
